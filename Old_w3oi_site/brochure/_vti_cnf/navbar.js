699d4fc28f36013119f37d590b09e078f99c6da6
W3AMD <kc3ase@gmail.com>
2016-02-07 22:37:03 -0500

    Remove old

--- Old_w3oi_site/brochure/_vti_cnf/navbar.js
+++
@@ -1,4 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|15 Dec 2004 15:18:14 -0000
-vti_extenderversion:SR|5.0.2.2634
-vti_backlinkinfo:VX|brochure/brochure1_nav.html brochure/brochure2_nav.html
--- Old_w3oi_site/scgi-bin/_vti_cnf/comments.pl
+++
@@ -1,4 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|26 Dec 2004 02:25:02 -0000
-vti_extenderversion:SR|5.0.2.2634
-vti_backlinkinfo:VX|edit-dir/comments2.htm
