8c2b1e6d3caa02ccf3ab1b660e48cb85e6cfd18e
W3AMD <kc3ase@gmail.com>
2016-02-13 11:11:20 -0500

    Query announcements changed for either mysql functions

--- app/queryannouncements.php
+++ app/queryannouncements.php
@@ -9,24 +9,12 @@ if (function_exists('nukeMagicQuotes')) {
 $conn = dbConnect();
 $currdate = date("Y-m-d", time());
 $sql = 'SELECT description FROM announcements WHERE startdate <= "'.$currdate.'" and enddate >= "'.$currdate.'" and active = "1" ORDER BY displayorder, enddate';
-//$result = mysql_query( $sql ) or die ( mysql_error() );
-$oldphp = getdbVersion();
-if($oldphp)
-{
-   $result = mysql_query($sql) or die(mysql_error());
-}
-else
-{
-   $result = mysqli_query($conn, $sql) or die(mysqli_error());
-}
+$result = _mysql_query($conn,$sql) ;
 $strReturn = '';
-while (($oldphp)? $row = mysql_fetch_assoc($result)
-      : $row = mysqli_fetch_assoc($result)) {
+while ($row = _mysql_fetch_assoc($conn1,$result)) {
 	$strReturn .= '<p>'.$row['description'].'</p>';
 }
 $strReturn .= '';
-
 print($strReturn);
-//print($sql);
 
 ?>
