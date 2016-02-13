163da3286b8d94d9cc0e0e0ec8b1b10ec7663113
W3AMD <kc3ase@gmail.com>
2016-02-13 17:34:16 -0500

    VOX Manager Update

--- app/voxeditorlogin.php
+++ app/voxeditorlogin.php
@@ -6,28 +6,34 @@ use_unit("extctrls.inc.php");
 use_unit("stdctrls.inc.php");
 include('../includes/sqlfunctions.inc.php');
 include('../includes/connection.inc.php');
-
+echo 'test';
 //Class definition
 class VOXEditorLogin extends Page
 {
-   public $Label1 = null;
-   public $Label2 = null;
-   public $Username = null;
-   public $Label3 = null;
-   public $Password = null;
-   public $LogIn = null;
     public $LoginStatus = null;
+    public $Panel1 = null;
+    public $Label1 = null;
+    public $Label2 = null;
+    public $Username = null;
+    public $Label3 = null;
+    public $Password = null;
+    public $LogIn = null;
+    public $Panel2 = null;
+    public $AddButton = null;
+    public $Panel3 = null;
+    public $Upload1 = null;
    function LogInClick($sender, $params)
    {
       //check the user log in against the database
-      $this->LoginStatus->Font->Color=Blue;
-      $this->LoginStatus->Caption='Logging In';
       $dbconnection = dbConnectOtherUsers($this->Username->Text,
       $this->Password->Text);
+      //check for connection success
       if($dbconnection)
       {
          $this->LoginStatus->Font->Color=Green;
          $this->LoginStatus->Caption='Success';
+         //disable the first panel
+
       }
       else
       {
---
+++ app/voxeditorlogin.xml.php
@@ -0,0 +1,129 @@
+<?php
+<object class="VOXEditorLogin" name="VOXEditorLogin" baseclass="Page">
+  <property name="Animations">a:0:{}</property>
+  <property name="Background"></property>
+  <property name="Caption">VOX Editor Login</property>
+  <property name="Height">348</property>
+  <property name="HiddenFields">a:0:{}</property>
+  <property name="Name">VOXEditorLogin</property>
+  <property name="UseAjax">1</property>
+  <property name="Width">409</property>
+  <object class="Panel" name="Panel1" >
+    <property name="Animations">a:0:{}</property>
+    <property name="Caption">Panel1</property>
+    <property name="Height">171</property>
+    <property name="Left">5</property>
+    <property name="Name">Panel1</property>
+    <property name="Width">395</property>
+    <object class="Label" name="Label1" >
+      <property name="Alignment">agCenter</property>
+      <property name="Animations">a:0:{}</property>
+      <property name="Caption">VOX Editor Login</property>
+      <property name="Font">
+      <property name="Color">Blue</property>
+      <property name="Size">20px</property>
+      </property>
+      <property name="Height">27</property>
+      <property name="Left">12</property>
+      <property name="Name">Label1</property>
+      <property name="ParentFont">0</property>
+      <property name="Top">16</property>
+      <property name="Width">372</property>
+    </object>
+    <object class="Label" name="Label2" >
+      <property name="Animations">a:0:{}</property>
+      <property name="Caption">Username:</property>
+      <property name="Height">13</property>
+      <property name="Left">28</property>
+      <property name="Name">Label2</property>
+      <property name="Top">64</property>
+      <property name="Width">59</property>
+    </object>
+    <object class="Edit" name="Username" >
+      <property name="Animations">a:0:{}</property>
+      <property name="Height">20</property>
+      <property name="Left">100</property>
+      <property name="Name">Username</property>
+      <property name="Text">w3oivoxeditor</property>
+      <property name="Top">64</property>
+      <property name="Width">121</property>
+    </object>
+    <object class="Label" name="Label3" >
+      <property name="Animations">a:0:{}</property>
+      <property name="Caption">Password:</property>
+      <property name="Height">13</property>
+      <property name="Left">28</property>
+      <property name="Name">Label3</property>
+      <property name="Top">110</property>
+      <property name="Width">59</property>
+    </object>
+    <object class="Edit" name="Password" >
+      <property name="Animations">a:0:{}</property>
+      <property name="Height">17</property>
+      <property name="InputType">cePassword</property>
+      <property name="Left">100</property>
+      <property name="Name">Password</property>
+      <property name="Text">O63dtg!4</property>
+      <property name="Top">106</property>
+      <property name="Width">121</property>
+    </object>
+    <object class="Button" name="LogIn" >
+      <property name="Animations">a:0:{}</property>
+      <property name="Caption">Log In</property>
+      <property name="Height">25</property>
+      <property name="Left">250</property>
+      <property name="Name">LogIn</property>
+      <property name="Top">73</property>
+      <property name="Width">75</property>
+      <property name="OnClick">LogInClick</property>
+    </object>
+    <object class="Label" name="LoginStatus" >
+      <property name="Alignment">agCenter</property>
+      <property name="Animations">a:0:{}</property>
+      <property name="Font">
+      <property name="Color">Crimson</property>
+      </property>
+      <property name="Height">19</property>
+      <property name="Left">16</property>
+      <property name="Name">LoginStatus</property>
+      <property name="ParentFont">0</property>
+      <property name="Top">138</property>
+      <property name="Width">363</property>
+    </object>
+  </object>
+  <object class="Panel" name="Panel2" >
+    <property name="Animations">a:0:{}</property>
+    <property name="Caption">Panel2</property>
+    <property name="Height">59</property>
+    <property name="Left">5</property>
+    <property name="Name">Panel2</property>
+    <property name="Top">176</property>
+    <property name="Width">395</property>
+    <object class="Button" name="AddButton" >
+      <property name="Animations">a:0:{}</property>
+      <property name="Caption">Add</property>
+      <property name="Height">25</property>
+      <property name="Left">11</property>
+      <property name="Name">AddButton</property>
+      <property name="Top">17</property>
+      <property name="Width">75</property>
+    </object>
+  </object>
+  <object class="Panel" name="Panel3" >
+    <property name="Animations">a:0:{}</property>
+    <property name="Caption">Panel3</property>
+    <property name="Height">91</property>
+    <property name="Name">Panel3</property>
+    <property name="Top">240</property>
+    <property name="Width">400</property>
+    <object class="Upload" name="Upload1" >
+      <property name="Animations">a:0:{}</property>
+      <property name="Height">21</property>
+      <property name="Left">16</property>
+      <property name="Name">Upload1</property>
+      <property name="Top">16</property>
+      <property name="Width">260</property>
+    </object>
+  </object>
+</object>
+?>
