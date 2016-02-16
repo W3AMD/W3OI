4398da2b91299166a2dcd26d14713eb12d325ccb
igorkasriel <igor@kasriel.net>
2016-02-15 21:16:49 -0500

    Image gallery working
    Also some cleanup

--- app/getimages.php
+++ app/getimages.php
@@ -4,8 +4,6 @@
 
 //see lvarc.css for visual display and orientation classes
 
-include('../layout/modalimages.inc.php');
-
 class Img {
    public $url = "";
    public $thumbUrl = "";
@@ -22,20 +20,15 @@ function listFolderFiles($dir, $currentdir, $imagefiles)
          {
             $pathfile = 'clubphotos/' . $currentdir . '/' . $ff;
             $previewfile = 'clubphotos/' . $currentdir . '/PREVIEWS/' . $ff;
-            // echo '<li class="event_image">' . '<a href="' . $pathfile . '" target="_blank">' . $ff . '</a></li>';
 
             // echo '<a href="' . $pathfile . '" width=900 height=600 alt="" border="0" class="lytebox" data-lyte-options="group:' . $currentdir . '">';
             // echo '<span style="width: 180px; height: 180px; padding: 10px;">';
             // echo '<img src="' . $previewfile . '" alt="" border="0">';
             // echo '<span></a>';
 
-            // echo '<div><span>' . $pathfile . '</span> : ';
-            // echo '<span>' . $previewfile . '</span></div>';
-
             $currentimage = new Img();
             $currentimage->url = $pathfile;
             $currentimage->thumbUrl = $previewfile;
-            echo json_encode($currentimage);
 
             array_push($imagefiles, $currentimage);
          }
@@ -43,28 +36,16 @@ function listFolderFiles($dir, $currentdir, $imagefiles)
          {
             // echo '<div class="event_name">' . $ff . '</div>';
             // echo '<ul>';
-            listFolderFiles($dir . '/' . $ff, $ff, $imagefiles);
+            $imagefiles = listFolderFiles($dir . '/' . $ff, $ff, $imagefiles);
             // echo '</ul>';
          }
       }
    }
+   return $imagefiles;
 }
-
-//create an array and search for vox issues
-//the array stores the date of the issue from the month/year in the file
 
 $imagefiles = array();
 $imagefiles = listFolderFiles('../clubphotos', NULL, $imagefiles);
-// echo $imagefiles;
 echo json_encode($imagefiles);
 
-while(count($imagefiles))
-{
-   foreach($imagefiles as $k=>$v)
-   {
-      $pathkey = $k + 1;
-      echo '<div>' . $pathkey . '</div>';
-      echo '<div>' . $imagefiles[$pathkey] . '</div>';
-   }
-}
 ?>
--- js/controllers/gallerycontroller.js
+++ js/controllers/gallerycontroller.js
@@ -7,13 +7,6 @@ myApp.controller('GalleryController', [ '$scope', '$rootScope', '$http', 'Lightb
 
 		$http(req).success(function(data){
 			$scope.images = data;
-			// $scope.images = [
-			// 					{'url': 'clubphotos/Field Day 2015/DSCN2353.JPG', 'thumbUrl': 'clubphotos/Field Day 2015/PREVIEWS/DSCN2353.JPG'},
-			// 					{'url': 'clubphotos/Field Day 2015/DSCN2355.JPG', 'thumbUrl': 'clubphotos/Field Day 2015/PREVIEWS/DSCN2355.JPG'},
-			// 					{'url': 'clubphotos/Field Day 2015/DSCN2356.JPG', 'thumbUrl': 'clubphotos/Field Day 2015/PREVIEWS/DSCN2356.JPG'},
-			// 					{'url': 'clubphotos/Field Day 2015/DSCN2358.JPG', 'thumbUrl': 'clubphotos/Field Day 2015/PREVIEWS/DSCN2358.JPG'}
-			// 				];
-			//console.log(data);
 			$scope.openLightboxModal = function (index) {
 				Lightbox.openModal($scope.images, index);
 			};
