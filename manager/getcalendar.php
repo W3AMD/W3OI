393e8cc201b328e390bba77bc992a49bd16f3526
W3AMD <kc3ase@gmail.com>
2016-02-24 21:03:38 -0500

    Final attempt
    No die

--- manager/getcalendar.php
+++ manager/getcalendar.php
@@ -7,25 +7,25 @@ echo 'client<br>';
 require_once '../googleapi/src/Google/Client.php';
 echo 'calendar<br>';
 require_once '../googleapi/src/Google/Service/Calendar.php';
- //password notasecret
 
-try {
-$client_id = 'c48bee008e4ad0cdd9b7b2b36cc23c4847fd4f4f';
-$client_email = 'calanderservice@w3oi-1231.iam.gserviceaccount.com';
-$private_key = file_get_contents('w3oicalendar.p12');
-$scopes = array('https://www.googleapis.com/auth/sqlservice.admin');
-$credentials = new Google_Auth_AssertionCredentials(
-    $client_email,
-    $scopes,
-    $private_key
-);
-$client = new Google_Client();
-$client->setAssertionCredentials($credentials);
-if ($client->getAuth()->isAccessTokenExpired()) {
-  $client->getAuth()->refreshTokenWithAssertion();
+try
+{
+   $client_email = 'calanderservice@w3oi-1231.iam.gserviceaccount.com';
+   $private_key = file_get_contents('w3oicalendar.p12');
+   $password = 'notasecret';
+   $scopes = array('https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/calendar.readonly');
+   $credentials = new Google_Auth_AssertionCredentials(
+   $client_email,$scopes,$private_key,$password);
+   $client = new Google_Client();
+   $client->setAssertionCredentials($credentials);
+   if($client->getAuth()->isAccessTokenExpired())
+   {
+      $client->getAuth()->refreshTokenWithAssertion();
+   }
 }
-}
-catch (exception $e) {
-echo $e;
+catch(exception$e)
+{
+   echo $e;
 }
 ?>
