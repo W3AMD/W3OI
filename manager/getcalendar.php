59e9ef9e4c164895bb7d998565815c3e0fefd6dc
W3AMD <kc3ase@gmail.com>
2016-02-24 19:57:35 -0500

    Revert "Revert "Goog Calendar""
    This reverts commit 1f5863270f053b1421123be4b6f06634aaa6d9ed.

--- manager/getcalendar.php
+++ manager/getcalendar.php
@@ -8,58 +8,67 @@ require_once '../googleapi/src/Google/Client.php';
 echo 'calendar<br>';
 require_once '../googleapi/src/Google/Service/Calendar.php';
 
-echo 'create client<br>';
 
+$client_id = '100246733343874029792';
+$Email_address = 'calanderservice@w3oi-1231.iam.gserviceaccount.com';
+echo 'create google client<br>';
 $client = new Google_Client();
+echo 'set application name<br>';
+$client->setApplicationName("Client_Calendar");
 echo 'load key file<br>';
-try {
-$client->setAuthConfigFile('client_secret.json');
+try
+{
+   $client->setAuthConfigFile('client_secret.json');
 }
-catch (Google_Exception $e) {
-echo 'Goog says: ' . $e;
+catch(Google_Exception$e)
+{
+   echo 'Goog says: ' . $e;
 }
-/*
-//$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
-
+echo 'create client<br>';
 $client = new Google_Client();
+echo 'set app name<br>';
 $client->setApplicationName("W3OICalender");
-*/
-/*
-if (isset($_SESSION['token'])) {
- $client->setAccessToken($_SESSION['token']);
-}
+//$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
+echo 'check session<br>';
 
-$key = file_get_contents($p12);
+if(isset($_SESSION['token']))
+{
+   echo 'success with token<BR>';
+   $client->setAccessToken($_SESSION['token']);
+}
+$key = file_get_contents('client_secret.json');
 $client->setClientId($client_id);
+echo 'check authorization<br>';
 $cred = new Google_Auth_AssertionCredentials(
-    $service_account,
-    array('https://www.googleapis.com/auth/calendar'),
-    $key);
-
+$service_account,
+array('https://www.googleapis.com/auth/calendar'),
+$key);
+echo 'assert creds<br>';
 $client->setAssertionCredentials($cred);
-if($client->getAuth()->isAccessTokenExpired()) {
-  $client->getAuth()->refreshTokenWithAssertion($cred);
+if($client->getAuth()->isAccessTokenExpired())
+{
+   echo 'expired<br>';
+   $client->getAuth()->refreshTokenWithAssertion($cred);
 }
+/*
 $_SESSION['service_token'] = $client->getAccessToken();
-
 $cal = new Google_Service_Calendar($client);
-
 $events = $cal->calendarList->listCalendarList();
 echo "<pre>";
 print_r($events);
 echo"</pre>";
 
 while(true) {
-    foreach ($events->getItems() as $event) {
-    echo $event->getSummary();
-        print_r($event);
-  }
-  $pageToken = $events->getNextPageToken();
-  if ($pageToken) {
-    $optParams = array('pageToken' => $pageToken);
-    $events = $service->calendarList->listCalendarList($optParams);
-  } else {
-    break;
-  }
+foreach ($events->getItems() as $event) {
+echo $event->getSummary();
+print_r($event);
+}
+$pageToken = $events->getNextPageToken();
+if ($pageToken) {
+$optParams = array('pageToken' => $pageToken);
+$events = $service->calendarList->listCalendarList($optParams);
+} else {
+break;
+}
 } */
 ?>
