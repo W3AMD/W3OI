4de8b9aaa4fe27a84a9f8204198c8e11e9743b96
W3AMD <kc3ase@gmail.com>
2016-02-24 20:34:26 -0500

    Google calendar
    Strike 3.

--- manager/getcalendar.php
+++ manager/getcalendar.php
@@ -7,70 +7,25 @@ echo 'client<br>';
 require_once '../googleapi/src/Google/Client.php';
 echo 'calendar<br>';
 require_once '../googleapi/src/Google/Service/Calendar.php';
-//client id: 863828392170-moqrthsgtg2gen6tgtq3s85h3nl68mgd.apps.googleusercontent.com
+ //password notasecret
 
-
-
-$client_id = '863828392170-moqrthsgtg2gen6tgtq3s85h3nl68mgd.apps.googleusercontent.com';
-$Email_address = 'calanderservice@w3oi-1231.iam.gserviceaccount.com';
-echo 'create google client<br>';
+try {
+$client_id = 'c48bee008e4ad0cdd9b7b2b36cc23c4847fd4f4f';
+$client_email = 'calanderservice@w3oi-1231.iam.gserviceaccount.com';
+$private_key = file_get_contents('w3oicalendar.p12');
+$scopes = array('https://www.googleapis.com/auth/sqlservice.admin');
+$credentials = new Google_Auth_AssertionCredentials(
+    $client_email,
+    $scopes,
+    $private_key
+);
 $client = new Google_Client();
-echo 'set application name<br>';
-$client->setApplicationName("Client_Calendar");
-echo 'load key file<br>';
-try
-{
-   $client->setAuthConfigFile('client_secret.json');
+$client->setAssertionCredentials($credentials);
+if ($client->getAuth()->isAccessTokenExpired()) {
+  $client->getAuth()->refreshTokenWithAssertion();
 }
-catch(Google_Exception$e)
-{
-   echo 'Goog says: ' . $e;
 }
-echo 'create client<br>';
-$client = new Google_Client();
-echo 'set app name<br>';
-$client->setApplicationName("W3OICalender");
-//$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
-echo 'check session<br>';
-
-if(isset($_SESSION['token']))
-{
-   echo 'success with token<BR>';
-   $client->setAccessToken($_SESSION['token']);
+catch (exception $e) {
+echo $e;
 }
-$key = file_get_contents('client_secret.json');
-$client->setClientId($client_id);
-echo 'check authorization<br>';
-$cred = new Google_Auth_AssertionCredentials(
-$service_account,
-array('https://www.googleapis.com/auth/calendar'),
-$key);
-echo 'assert creds<br>';
-$client->setAssertionCredentials($cred);
-if($client->getAuth()->isAccessTokenExpired())
-{
-   echo 'expired<br>';
-   $client->getAuth()->refreshTokenWithAssertion($cred);
-}
-/*
-$_SESSION['service_token'] = $client->getAccessToken();
-$cal = new Google_Service_Calendar($client);
-$events = $cal->calendarList->listCalendarList();
-echo "<pre>";
-print_r($events);
-echo"</pre>";
-
-while(true) {
-foreach ($events->getItems() as $event) {
-echo $event->getSummary();
-print_r($event);
-}
-$pageToken = $events->getNextPageToken();
-if ($pageToken) {
-$optParams = array('pageToken' => $pageToken);
-$events = $service->calendarList->listCalendarList($optParams);
-} else {
-break;
-}
-} */
 ?>
