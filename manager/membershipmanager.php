79c57d6ff46e47dcc990e5d214fb2114c3685d12
W3AMD <kc3ase@gmail.com>
2016-03-22 12:20:29 -0400

    Framework

--- manager/membershipmanager.php
+++ manager/membershipmanager.php
@@ -18,14 +18,10 @@
       <ul class="nav navbar-nav">
         <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Member Editor<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
+            <li><a href="#">Add</a></li>
+            <li><a href="#">Update</a></li>
+            <li><a href="#">Add Payment Record</a></li>
+            </ul>
         </li>
       </ul>
       <form class="navbar-form" role="search">
