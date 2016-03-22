5c1af2fd4bd762899f2ac565869dc115ab7367de
W3AMD <kc3ase@gmail.com>
2016-03-22 12:09:12 -0400

    Clean sweep

--- manager/membershipmanager.php
+++ manager/membershipmanager.php
@@ -18,17 +18,24 @@
       <ul class="nav navbar-nav">
         <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Member Editor<span class="caret"></span></a>
           <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
           </ul>
         </li>
       </ul>
       <form class="navbar-form" role="search">
         <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search For Member (Lastname or Call sign)">
+          <input type="text" class="form-control" placeholder="Search For Member">
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Log Out</a></li>
+        <li></li>
         <li class="dropdown">
 <ul class="dropdown-menu">
             <li><a href="#">Action</a></li>
